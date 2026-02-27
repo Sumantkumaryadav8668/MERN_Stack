040925638fc5059b15e639567403ba4db95a0065
Sumant Kumar Yadav <ersumantkumaryadav8668@gmail.com>
2026-02-27 08:44:42 +0530

    set timer

---
+++ 3_JS/Day17/setTimer.js
@@ -0,0 +1,8 @@
+
+console.log('hello skumant kumar');
+
+setTimeout(()=>{  // setTimeout me time set hi ya nahi yah bad me hi execute hota hai
+    console.log('How are you ?')
+},0)  // timing set for run code
+
+console.log('I am fine and you ?')
