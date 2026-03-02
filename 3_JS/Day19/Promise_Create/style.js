39eda106dc721fd5ca486c60004ade1cbf59f72e
Sumant Kumar Yadav <ersumantkumaryadav8668@gmail.com>
2026-03-03 00:24:50 +0530

    create promise

---
+++ 3_JS/Day19/Promise_Create/style.js
@@ -0,0 +1,40 @@
+//    // Promise create 
+
+// const p1 = new Promise((resolve,reject)=>{
+//     resolve("hello");
+// })  
+
+//         // Promise ko read karne ke liye use karte hai
+// .then((data)=>{     // resolve ke case me
+//     console.log(data);
+// }) 
+// .catch((response)=>{   // reject ke case me
+//     console.log(response);
+// })
+
+// // console.log(p1);
+
+
+
+
+// const p1 = new Promise((resolve,reject)=>{
+//     resolve(2);
+// })  
+
+//         // Promise ko read karne ke liye use karte hai
+// .then((data)=>{ return {k1:2*data, k2:" samir"}})
+// .then((data)=>{
+//     console.log(data.k2)
+// }) 
+// .catch((response)=>{   // reject ke case me
+//     console.log(response);
+// })
+
+// console.log(p1);
+
+
+
+
+// (a,b)=>a+b
+// (a,b)=>(a+b)
+// (a,b)=>{ return a+b}
