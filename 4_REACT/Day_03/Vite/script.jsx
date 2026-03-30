fbe103881f1e98448e7d0e4eb4b5c3b70271b102
Sumant Kumar Yadav <ersumantkumaryadav8668@gmail.com>
2026-03-30 14:55:09 +0530

    vite

---
+++ 4_REACT/Day_03/Vite/script.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const element = <h1>Hello coder army</h1>
+
+// ReactDOM.createRoot(document.getElementById('root')).render(element);
+
+        const element = <h1>Hello coder army</h1>;
+        ReactDOM.createRoot(document.getElementById('root')).render(element);
