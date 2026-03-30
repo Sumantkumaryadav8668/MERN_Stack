563885ec6a2492ca99919fcf061266990a198de6
Sumant Kumar Yadav <ersumantkumaryadav8668@gmail.com>
2026-03-30 12:25:12 +0530

    react

---
+++ 4_REACT/Day_03/First/script.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const element = <>
+    <h1>Hello Coder Army</h1>
+    <h2>kaise hai app</h2>
+</>
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(element);
