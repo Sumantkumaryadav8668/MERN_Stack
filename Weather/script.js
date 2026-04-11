8657d99c4403469e53e036f6a49f88496acbc746
Sumant Kumar Yadav <ersumantkumaryadav8668@gmail.com>
2026-04-11 21:49:57 +0530

    Weather

--- Weather/script.js
+++ Weather/script.js
@@ -1,2 +1,16 @@
+const axios = require('axios')
+require('dotenv/config')
 
-// const city = document.getElementById("city").value;
+const getInfo = async ()=>{
+
+    try{
+        console.log(process.env.WEATHER_KEY)
+        // const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=${process.env.WEATHER_KEY}`)
+        // console.log(result)
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+
+getInfo()
