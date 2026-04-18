1e40e289fd7b142a95ba08dc89c6703feef19a0f
Sumant Kumar Yadav <ersumantkumaryadav8668@gmail.com>
2026-04-18 21:17:55 +0530

    react modual

--- 4_REACT/Day_04/Day_04.1/Counter/src/App.jsx
+++ 4_REACT/Day_04/Day_04.1/Counter/src/App.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react"
 
 function App(){
-  let counter =0;
-  function IncreaseNum(){
-    counter++;
+  // let counter =0;
+  // function IncreaseNum(){
+  //   counter++;
     
-    const para = document.querySelector('p');
-    para.textContent = `Counter: ${counter}`
-    const buttan = document.getElementById('increase')
-    buttan.textContent= `Increase ${counter}`
-    console.log(counter);
-  }
+  //   const para = document.querySelector('p');
+  //   para.textContent = `Counter: ${counter}`
+  //   const buttan = document.getElementById('increase')
+  //   buttan.textContent= `Increase ${counter}`
+  //   console.log(counter);
+  // }
 
   // function Decrease(){
   //   counter--;
@@ -20,11 +21,15 @@ function App(){
   //   console.log(counter);
   // }
 
+
+  const[Count,setCount] = useState(0);
+
+
   return(
     <>
-    <p>Counter: {counter}</p>
-    <button id="increase" onClick={IncreaseNum}>Increase {counter}</button>
-    {/* <button id="decrease" onClick={Decrease}>Decrease {counter}</button> */}
+    <p>Counter: {Count}</p>
+    <button id="increase" onClick={()=> setCount(Count+1)}>Increase {Count}</button>
+    <button id="decrease" onClick={()=>setCount(Count-1)}>Decrease {Count}</button>
     </>
   )
 }
